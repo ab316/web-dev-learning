521f823658a222b6684a4850c57d4911713752c1
Abdullah Baig <abdullahbaig456@gmail.com>
2021-12-12 00:44:05 +0100

    React : Task Tracker : React router

--- frontend/react/1-task-tracker/src/App.js
+++ frontend/react/1-task-tracker/src/App.js
@@ -1,7 +1,10 @@
 import {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/Header';
 import {Tasks} from './components/Tasks';
 import {AddTask} from './components/AddTask';
+import {Footer} from './components/Footer';
+import {About} from './components/About';
 
 const serverUrl = 'http://localhost:5000';
 
@@ -62,16 +65,33 @@ function App() {
 
   return (
     <div className="container">
-      <Header
-        onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask}
-      />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        'No tasks to show'
-      )}
+      <Router>
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  'No tasks to show'
+                )}
+                <Footer />
+              </>
+            }
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
