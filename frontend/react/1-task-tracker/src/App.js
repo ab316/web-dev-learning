b410ec40bdd23071fb0c3b266c90f96e296460a9
Abdullah Baig <abdullahbaig456@gmail.com>
2021-12-11 23:11:18 +0100

    React : Task Tracker : Add json server

--- frontend/react/1-task-tracker/src/App.js
+++ frontend/react/1-task-tracker/src/App.js
@@ -1,48 +1,63 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Header from './components/Header';
 import {Tasks} from './components/Tasks';
 import {AddTask} from './components/AddTask';
 
+const serverUrl = 'http://localhost:5000';
+
 function App() {
   // State is immutable. You have to set it again (using the set method) when needed and it gets propagated down to the components
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctor',
-      day: '13th Dec at 12:00',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Groceries',
-      day: '13th Dec at 18:00',
-      reminder: false,
-    },
-    {
-      id: 3,
-      text: 'Daily',
-      day: '13th Dec at 09:00',
-      reminder: true,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = {id, ...task};
+  const fetchTasks = async () => {
+    const res = await fetch(`${serverUrl}/tasks`);
+    const data = await res.json();
+    return data;
+  };
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`${serverUrl}/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  };
+
+  useEffect(() => {
+    // We can not provide an async function to useEffect, so we make a async function inside it and call it instead
+    (async () => {
+      const tasks = await fetchTasks();
+      setTasks(tasks);
+    })();
+    // [] is required to run the effect only once when the component loads
+  }, []);
+
+  const addTask = async (task) => {
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = {id, ...task};
+    const res = await fetch(`${serverUrl}/tasks`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(task),
+    });
+    const newTask = await res.json();
     setTasks([...tasks, newTask]);
   };
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`${serverUrl}/tasks/${id}`, {method: 'DELETE'});
     setTasks(tasks.filter((t) => t.id !== id));
   };
 
-  const toggleReminder = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? {...task, reminder: !task.reminder} : task,
-      ),
-    );
+  const toggleReminder = async (id) => {
+    const task = await fetchTask(id);
+    const updatedTask = {...task, reminder: !task.reminder};
+    const res = await fetch(`${serverUrl}/tasks/${id}`, {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(updatedTask),
+    });
+    const newTask = await res.json();
+    setTasks(tasks.map((task) => (task.id === id ? newTask : task)));
   };
 
   return (
