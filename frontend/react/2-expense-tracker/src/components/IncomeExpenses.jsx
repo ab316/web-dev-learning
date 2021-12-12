9a20e97d0baeeab138b50c9f59ecce0f57c12d42
Abdullah Baig <abdullahbaig456@gmail.com>
2021-12-12 11:55:02 +0100

    React : Expense Tracker : Add UI

---
+++ frontend/react/2-expense-tracker/src/components/IncomeExpenses.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const IncomeExpenses = () => {
+  return (
+    <div className="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p id="money-plus" className="money plus">
+          +$0.00
+        </p>
+      </div>
+
+      <div>
+        <h4>Expense</h4>
+        <p id="money-minu" className="money minus">
+          -$0.00
+        </p>
+      </div>
+    </div>
+  );
+};
