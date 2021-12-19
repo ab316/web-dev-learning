241c3cb3103aa37e781e9dced99528a701c25e30
Abdullah Baig <abdullahbaig456@gmail.com>
2021-12-19 21:21:36 +0100

    Frontend : React : Styled Components : Basics

---
+++ frontend/react/4-styled-components/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import {StyledHeader} from './styles/Header.styled';
+
+export const Header = () => {
+  return (
+    <StyledHeader>
+      <h1>Hubble</h1>
+    </StyledHeader>
+  );
+};
---
+++ frontend/react/4-styled-components/src/components/styles/styled.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      header: string;
+      body: string;
+    };
+  }
+}
