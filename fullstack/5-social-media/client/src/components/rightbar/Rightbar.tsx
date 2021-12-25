6a75079da1f4b0c3b4f3a94d5cc4b65c27222d16
Abdullah Baig <abdullah.baig@afry.com>
2021-12-25 22:17:02 +0100

    Fullstack : Social Media : Rightbar

--- fullstack/5-social-media/client/src/components/rightbar/Rightbar.tsx
+++ fullstack/5-social-media/client/src/components/rightbar/Rightbar.tsx
@@ -1,7 +1,62 @@
 import './rightbar.css';
 
 const rightbar = () => {
-  return <div className="rightbar">Right bar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="assets/birthday.png" alt="Birthday" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Tom</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+
+        <img src="assets/ad.jpg" alt="Ad" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Frields</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/3.png" alt="person" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Ragnar Lodbrok</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/3.png" alt="person" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Squids Henderson</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/3.png" alt="person" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Chewy Sugar-Gold</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/3.png" alt="person" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Bad News Cocktoasten</span>
+          </li>
+
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="assets/person/3.png" alt="person" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Rock Candy Endicott</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default rightbar;
