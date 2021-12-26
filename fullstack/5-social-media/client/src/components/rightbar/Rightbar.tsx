2451f65e882acb8e319f968f25c11e34b4d897f2
Abdullah Baig <abdullah.baig@afry.com>
2021-12-26 19:51:15 +0100

    Fullstack : Social Media : Profile page

--- fullstack/5-social-media/client/src/components/rightbar/Rightbar.tsx
+++ fullstack/5-social-media/client/src/components/rightbar/Rightbar.tsx
@@ -1,11 +1,16 @@
-import './rightbar.css';
+import {FC} from 'react';
 import {Users} from '../../dummyData';
 import Online from '../online/Online';
+import './rightbar.css';
 
-const rightbar = () => {
-  return (
-    <div className="rightbar">
-      <div className="rightbarWrapper">
+interface IProps {
+  profile?: boolean;
+}
+
+const rightbar: FC<IProps> = ({profile}) => {
+  const HomeRightbar = () => {
+    return (
+      <>
         <div className="birthdayContainer">
           <img src="assets/birthday.png" alt="Birthday" className="birthdayImg" />
           <span className="birthdayText">
@@ -20,7 +25,55 @@ const rightbar = () => {
             <Online key={u.id} user={u} />
           ))}
         </ul>
-      </div>
+      </>
+    );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">My Information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">Gothenburg</span>
+          </div>
+
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">Lahore</span>
+          </div>
+
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship:</span>
+            <span className="rightbarInfoValue">Single</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">My Friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img src="assets/person/2.png" alt="Following" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">Some guy</span>
+          </div>
+
+          <div className="rightbarFollowing">
+            <img src="assets/person/3.png" alt="Following" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">Robot guy</span>
+          </div>
+
+          <div className="rightbarFollowing">
+            <img src="assets/person/4.png" alt="Following" className="rightbarFollowingImg" />
+            <span className="rightbarFollowingName">Funny guy</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper"></div>
+      {profile ? <ProfileRightbar /> : <HomeRightbar />}
     </div>
   );
 };
---
+++ fullstack/5-social-media/client/src/pages/profile/Profile.tsx
@@ -0,0 +1,34 @@
+import Feed from '../../components/feed/Feed';
+import Rightbar from '../../components/rightbar/Rightbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Topbar from '../../components/topbar/Topbar';
+import './profile.css';
+
+const Profile = () => {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img src="assets/post/4.jfif" alt="Cover" className="profileCoverImg" />
+              <img src="assets/person/1.png" alt="Profile" className="profileUserImg" />
+            </div>
+            <div className="profileInfo">
+              <h4 className="profileInfoName">John Doe</h4>
+              <span className="profileInfoDesc">Hello my friends</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
